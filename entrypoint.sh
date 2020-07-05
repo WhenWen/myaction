8aaca7562411f33c04f644b3bd99ad95582877d9
when <kaiyuewen3@gmail.com>
2020-07-05 12:28:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
 echo "start checking"
-suggestion=$(ls *.md|xargs proselint)
+#suggestion=$( ls *.md|xargs proselint )
+suggestion="haha"
 echo "::set-output name=suggestion::$suggestion"
