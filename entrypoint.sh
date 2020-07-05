ff4fe087dc3b46e089e917e759b5e7a23e51bffb
when <kaiyuewen3@gmail.com>
2020-07-05 13:09:21 +0800

    checking how can one find some shit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 cd "$GITHUB_WORKSPACE"
 echo "start checking"
-suggestion=$( ls *.md|xargs proselint )
+#suggestion=$( ls *.md|xargs proselint )
 #suggestion="haha"
+suggestion=$( find .type f -name '*.md' )
 echo "::set-output name=suggestion::$suggestion"
