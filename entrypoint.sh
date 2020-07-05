759889e9d672cf2b45bb233903c0df2eed63a525
when <kaiyuewen3@gmail.com>
2020-07-05 13:25:39 +0800

    can i find anything

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,5 @@ echo "start checking"
 #suggestion=$( ls *.md|xargs proselint )
 #suggestion="haha"
 suggestion=$( find . -name '*.md' )
+echo "$suggestion"
 echo "::set-output name=suggestion::$suggestion"
